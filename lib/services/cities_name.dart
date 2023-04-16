664af6ea17f5427458cb521baf112ebadd406f79
BashmailOmar <bashmailomar@gmail.com>
2023-04-16 10:01:51 +0300

    ""

--- lib/services/cities_name.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cities/cities.dart';
-import 'package:cities/model.dart';
-
-Future<List> getCitiesName() async {
-  final cities = await cities_auto();
-  final allCities = cities.all;
-  // final first_ten_cities = all_cities.take(10);
-  return allCities;
-}
